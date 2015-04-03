4fc4e2ee55164e069f4ea655436e1c76be63e38a
Fernando Frazão <ffrazao@gmail.com>
2015-04-03 14:17:22 -0300

    Reinicio deste projeto. Utilizando Node.js

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name aterwebApp
+ * @description
+ * # aterwebApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('aterwebApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
