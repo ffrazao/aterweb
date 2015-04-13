8a1986fc061e366bbf25f5b3e87b2f7482703569
Fernando Frazão <ffrazao@gmail.com>
2015-04-13 11:27:29 -0300

    itens salvos

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -17,52 +17,92 @@ var aterwebApp = angular.module('aterwebApp', [
 	'ngSanitize',
 	'ngTouch',
 	'ui.router',
-	'ui.bootstrap'
+	'ui.bootstrap',
+	'toastr',
+	'ngTable'
 ]);
 
-// ajustar o router da aplicação
-aterwebApp.config(function($stateProvider, $urlRouterProvider ) {
-	//
+aterwebApp.run(['$rootScope', '$state', '$stateParams', function ($rootScope, $state, $stateParams) {
+	$rootScope.estado = $state;
+	$rootScope.estadoParams = $stateParams;
+}]);
+
+aterwebApp.config(function($stateProvider, $urlRouterProvider, toastrConfig) {
+	// configurando o toastr - https://github.com/Foxandxss/angular-toastr
+	angular.extend(toastrConfig, {
+		allowHtml: true,
+		closeButton: true,
+		closeHtml: '<button>&times;</button>',
+		containerId: 'toast-container',
+		extendedTimeOut: 1000,
+		iconClasses: {
+			error: 'toast-error',
+			info: 'toast-info',
+			success: 'toast-success',
+			warning: 'toast-warning'
+		},
+		maxOpened: 0,
+		messageClass: 'toast-message',
+		newestOnTop: true,
+		onHidden: null,
+		onShown: null,
+		positionClass: 'toast-top-full-width',
+		preventDuplicates: false,
+		progressBar: true,
+		tapToDismiss: true,
+		target: 'body',
+		templates: {
+			toast: 'directives/toast/toast.html',
+			progressbar: 'directives/progressbar/progressbar.html'
+		},
+		timeOut: 5000,
+		titleClass: 'toast-title',
+		toastClass: 'toast'
+	});
+
+	// ajustar o router da aplicação
+
 	// For any unmatched url, redirect to /about
-	$urlRouterProvider.otherwise('/about');
+	$urlRouterProvider.otherwise('/');
 	//
 	// Now set up the states
 	$stateProvider
-	.state('about', {
-		url: '/about',
-		templateUrl: 'views/about.html'
+
+	.state('login', {
+		url: '/login',
+		templateUrl: 'views/login/login.html',
+		controller: 'LoginCtrl'
 	})
 
-	.state('about.list', {
-		url: '/list',
-		templateUrl: 'views/about.list.html',
-		controller: function($scope, $modal) {
-			$scope.items = ['A', 'List', 'Of', 'Items'];
-			$scope.exibe = function () {
-				$modal.open({
-					templateUrl: 'views/main.html',
-					size: 'lg',
-					resolve: {
-						items: function () {
-							return $scope.items;
-						}
-					}
-				});
-
-			};
-		}
-	})
-
-	.state('main', {
-		url: '/main',
-		templateUrl: 'views/main.html',
-		controller: 'MainCtrl'
+	.state('p', {
+		templateUrl: 'views/principal.html'
 	})
 	
-	.state('main.list', {
-		url: '/list',
-		templateUrl: 'views/main.list.html',
-		controller: 'MainListCtrl'
-	});
+	.state('p.bem-vindo', {
+		url: '/',
+		templateUrl: 'views/bem-vindo.html'
+	})
+
+	.state('p.modeloCadastro', {
+		url: '/modelo-cadastro',
+		templateUrl: 'views/_modelo-cadastro/_modelo-cadastro.html',
+		controller: 'ModeloCadastroCtrl'
+	})
+
+	.state('p.modeloCadastro.filtro', {
+		url: '/',
+		templateUrl: 'views/_modelo-cadastro/filtro.html'
+	})
+
+	.state('p.modeloCadastro.lista', {
+		url: '/lista',
+		templateUrl: 'views/_modelo-cadastro/lista.html'
+	})
+
+	.state('p.modeloCadastro.formulario', {
+		url: '/formulario/:id',
+		templateUrl: 'views/_modelo-cadastro/formulario.html'
+	})
+	;
 
 });
